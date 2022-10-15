1eb79fedc568dcae4fb00c57bf9f8ac290d89915
Ogechi Ikwunagu <ogeikwunagu@gmail.com>
2022-10-15 09:10:44 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Entry
+ *Return: 0 if successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
