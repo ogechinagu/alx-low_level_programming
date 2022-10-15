3120fffe06a8eb33c0d17fc6d325362df0e4f6ca
Ogechi Ikwunagu <ogeikwunagu@gmail.com>
2022-10-14 23:49:46 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The entry point for c
+ * Return: will be 0 if no errors
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
